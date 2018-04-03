4a6f1e3fc52b9f84ef25999e7fc6ade4829b239a
joka <admin@joka.org.ua>
2018-04-03 16:56:04 +0200

    added ability to set buy_trigger, buy_value, all_buy_value automatically

---
+++ ptwhitelistru_get_pairs.php
@@ -0,0 +1,121 @@
+<?php
+
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Middleware;
+use GuzzleHttp\TransferStats;
+use Jokaorgua\Monolog\Handler\TelegramHandler;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+date_default_timezone_set('GMT');
+//loading .env data
+$dotenv = new Dotenv\Dotenv(__DIR__);
+$dotenv->load();
+
+// create a log channel
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('php://stdout', Logger::DEBUG));
+$log->pushHandler(new TelegramHandler(getenv('TELEGRAM_BOT_TOKEN'), getenv('TELEGRAM_CHAT_ID'), Logger::ALERT));
+
+$container = [];
+$history = Middleware::history($container);
+
+$stack = HandlerStack::create();
+// Add the history middleware to the handler stack.
+$stack->push($history);
+$params = [
+    'base_uri' => 'http://ptwhitelist.ru/',
+    'timeout' => 5,
+    'cookies' => true,
+    'handler' => $stack
+];
+
+
+$guzzle = new GuzzleHttp\Client($params);
+
+//login to ptwhitelist.ru
+/**
+ * @var \GuzzleHttp\Psr7\Uri $url
+ */
+$url = '';
+$guzzle->request('POST', '/index.php?route=account/login/login',
+    [
+        'form_params' => [
+            'username' => getenv('PTWHITELISTRU_LOGIN'),
+            'pass' => getenv('PTWHITELISTRU_PASSWORD'),
+        ],
+        'on_stats' => function (TransferStats $stats) use (&$url) {
+            $url = $stats->getEffectiveUri();
+        }
+    ]);
+
+if ($url->getQuery() != 'route=common/home') {
+    $log->alert('Cant login to ptwhitelist.ru');
+    exit(1);
+}
+
+//send filter query
+$result = $guzzle->request('POST', '/index.php?route=common/home',
+    [
+        'form_params' => [
+            'para' => getenv('MARKET'),
+            'outdays' => getenv('PTWHITELISTRU_DAYS_TO_ANALYZE'),
+            'filtervol' => getenv('PTWHITELISTRU_VOLUME'),
+            'filtervoldays' => getenv('PTWHITELISTRU_VOLUME_DAYS'),
+            'filtervolcount' => getenv('PTWHITELISTRU_VOLUME_COUNT'),
+            'filternewcoin' => getenv('PTWHITELISTRU_NEW_COIN_MIN_DAYS'),
+            'filterpricedays' => getenv('PTWHITELISTRU_PRICE_DAYS'),
+            'filterpricepercent' => getenv('PTWHITELISTRU_PRICE_PERCENT'),
+            'filterpricepercentdown' => getenv('PTWHITELISTRU_PRICE_PERCENT_DOWN'),
+        ]
+    ]);
+$responseCode = $result->getBody()->getContents();
+$crawler = new \Symfony\Component\DomCrawler\Crawler($responseCode);
+$PAIRS = [];
+foreach ($crawler->filter('div.symboldiv.aligndiv > div[data-symb]') as $entry) {
+    $PAIRS[] = $entry->getAttribute('data-symb');
+}
+if (empty($PAIRS)) {
+    $log->alert('Found ZERO pairs for enabling. Will disable everything.');
+    exit(1);
+}
+//check if PT config file exists
+if (!file_exists(getenv('PT_ROOT_DIR') . 'trading' . DIRECTORY_SEPARATOR . 'PAIRS.properties')) {
+    $log->alert('PT pairs config doesnt exist. WFT???');
+    exit(1);
+}
+
+if (!is_readable(getenv('PT_ROOT_DIR') . 'trading' . DIRECTORY_SEPARATOR . 'PAIRS.properties')) {
+    $log->alert('PT pairs config it not readable.');
+    exit(1);
+}
+
+if (!is_writable(getenv('PT_ROOT_DIR') . 'trading' . DIRECTORY_SEPARATOR . 'PAIRS.properties')) {
+    $log->alert('PT pairs config is not writable.');
+    exit(1);
+}
+
+$pairsConfigData = file_get_contents(getenv('PT_ROOT_DIR') . 'trading' . DIRECTORY_SEPARATOR . 'PAIRS.properties');
+
+// ensure that ALL_sell_only_mode enabled
+if (!preg_match('#ALL_sell_only_mode\s+=\s+true#', $pairsConfigData)) {
+    $log->alert('Please enable ALL_sell_only_mode');
+    exit(1);
+}
+
+//remove all false sell modes
+
+$pairsConfigData = preg_replace("#^[A-Z]{5,8}_sell_only_mode\s+=\s+false#m", '', $pairsConfigData);
+$pairsConfigData = trim($pairsConfigData) . PHP_EOL . PHP_EOL;
+
+
+foreach ($PAIRS as $pair) {
+    $log->info($pair . strtoupper(getenv('MARKET')) . '_sell_only_mode = false');
+    $pairsConfigData .= PHP_EOL . $pair . getenv('MARKET') . '_sell_only_mode = false';
+}
+
+file_put_contents(getenv('PT_ROOT_DIR') . 'trading' . DIRECTORY_SEPARATOR . 'PAIRS.properties', $pairsConfigData);
+
+
--- set_by_mikes_index.php
+++ set_by_mikes_index.php
@@ -20,7 +20,7 @@ if (!empty(getenv('TELEGRAM_BOT_TOKEN'))) {
 }
 
 $api = new \Binance\API(getenv('BINANCE_API_KEY'), getenv('BINANCE_API_SECRET'));
-$symbols = $api->prevDay();
+$symbols = $api->prevDay(null);
 $symbolsData = [];
 //filter the market
 foreach ($symbols as $symbolEntry) {
@@ -32,10 +32,16 @@ usort($symbolsData, function ($a, $b) {
     return $b['priceChangePercent'] <=> $a['priceChangePercent'];
 });
 
-$lowestChangeData = $symbolsData[intval(getenv('SELL_VALUE_COIN_INDEX')) - 1];
+$lowestSellValueChangeData = $symbolsData[intval(getenv('SELL_VALUE_COIN_INDEX')) - 1];
+$lowestBuyValueChangeData = $symbolsData[intval(getenv('BUY_VALUE_COIN_INDEX')) - 1];
+$lowestBuyTriggerChangeData = $symbolsData[intval(getenv('BUY_TRIGGER_COIN_INDEX')) - 1];
 
 $sellValue = round(min(getenv('MAX_SELL_VALUE'),
-    max(getenv('MIN_SELL_VALUE'), $lowestChangeData['priceChangePercent'])), 2);
+    max(getenv('MIN_SELL_VALUE'), $lowestSellValueChangeData['priceChangePercent'])), 2);
+$buyValue = round(min(getenv('MAX_BUY_VALUE'),
+    max(getenv('MIN_BUY_VALUE'), $lowestSellValueChangeData['priceChangePercent'])), 2);
+$buyTrigger = round(min(getenv('MAX_BUY_TRIGGER'),
+    max(getenv('MIN_BUY_TRIGGER'), $lowestSellValueChangeData['priceChangePercent'])), 2);
 $trailingBuy = 0.2;
 if ($sellValue > 1 && $sellValue <= 3) {
     $trailingBuy = 0.3;
@@ -47,6 +53,8 @@ if ($sellValue > 1 && $sellValue <= 3) {
     $trailingBuy = 1;
 }
 $log->info('Sell value', [$sellValue]);
+$log->info('Buy value', [$buyValue]);
+$log->info('Buy trigger', [$buyTrigger]);
 $log->info('Trailing buy',[$trailingBuy]);
 $pairsConfigFilePath = getenv('PT_ROOT_DIR') . 'trading' . DIRECTORY_SEPARATOR . 'PAIRS.properties';
 $dcaConfigFilePath = getenv('PT_ROOT_DIR') . 'trading' . DIRECTORY_SEPARATOR . 'DCA.properties';
@@ -85,10 +93,13 @@ $pairsConfigData = file_get_contents($pairsConfigFilePath);
 $dcaConfigData = file_get_contents($dcaConfigFilePath);
 
 $dcaConfigData = preg_replace('#^sell_value\s+=\s+[-0-9.]+#m', 'sell_value = ' . $sellValue, $dcaConfigData);
-$dcaConfigData = preg_replace('#^trailing_profit\s+=\s+[-0-9.]+#m', 'trailing_profit = ' . $trailingBuy, $dcaConfigData);
+$dcaConfigData = preg_replace('#^buy_value\s+=\s+[-0-9.]+#m', 'buy_value = ' . $buyValue, $dcaConfigData);
+$dcaConfigData = preg_replace('#^buy_trigger\s+=\s+[-0-9.]+#m', 'buy_trigger = ' . $buyTrigger, $dcaConfigData);
 file_put_contents($dcaConfigFilePath, $dcaConfigData);
 
 $pairsConfigData = preg_replace('#^ALL_sell_value\s+=\s+[-0-9.]+#m', 'ALL_sell_value = ' . $sellValue,
+    $pairsConfigData);
+$pairsConfigData = preg_replace('#^ALL_buy_value\s+=\s+[-0-9.]+#m', 'ALL_sell_value = ' . $buyValue,
     $pairsConfigData);
 $pairsConfigData = preg_replace('#^ALL_trailing_profit\s+=\s+[-0-9.]+#m', 'ALL_trailing_profit = ' . $trailingBuy,
     $pairsConfigData);
